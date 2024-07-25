ffb3e4fafc8662a82b88048c499f37ba1a689536
Tanmay Patil <tany.patil77@gmail.com>
2024-07-26 00:03:28 +0530

    ast simple test

--- hello/ast/ast.go
+++ hello/ast/ast.go
@@ -53,9 +53,9 @@ func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 func (ls *LetStatement) String() string {
 	var out bytes.Buffer
 
-	out.WriteString(ls.TokenLiteral() + "")
+	out.WriteString(ls.TokenLiteral() + " ")
 	out.WriteString(ls.Name.String())
-	out.WriteString("=")
+	out.WriteString(" = ")
 	if ls.Value != nil {
 		out.WriteString(ls.Value.String())
 	}
